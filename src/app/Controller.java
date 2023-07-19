18de619c5ba21ad108f20fdcf8fe6c420c6c4e00
Roberto03082004 <rcallejas22415@coumbushs.com>
2023-07-19 12:29:23 -0400

    hghghghg

--- src/app/Controller.java
+++ src/app/Controller.java
@@ -1,2 +1,78 @@
-package app;public class Controller {
-}
+package app;
+
+import things.Items;
+
+public class Controller {
+    public static void main(String[] args){
+        yourInfoHeader();
+
+        Items item1 = new Items("Apple", 1.29);
+        item1.info(true);
+        System.out.println();
+        item1.info(false);
+        Items item2 = new Items("Milk", 5.69, 25, 0.153);
+        Items item3 = new Items("Water", 2.19, 50, 0.05);
+        item2.info(true);
+        item3.info(true);
+        System.out.println();
+
+        //Step 8
+        if (item2.getPrice() > item1.getPrice()) {
+            System.out.println("Milk has greater price. Price of Milk: " + item2.getPrice());
+        } else {
+            System.out.println("Apple has greater or equal price. Price of Apple: " + item1.getPrice());
+        }//end of if statement
+        System.out.println();
+
+        //Step 9
+        if(item1.getDiscount() < 0.05){
+            System.out.println("Apple discount is less than 0.05. The discount is " + item1.getDiscount());
+        } else if (item1.getDiscount() < 0.10) {
+            System.out.println("Apple discount is less than 0.10. The discount is " + item1.getDiscount());
+        }else if(item1.getDiscount() < 0.15) {
+            System.out.println("Apple discount is less than 0.15. The discount is " + item1.getDiscount());
+        }else{
+            System.out.printf("Apple discount is greater or equal to 0.15. The discount is %.3f\n", item1.getDiscount());
+        }//end of if statement
+        System.out.println();
+
+        //Step 10
+        if(item3.getPrice() >= 3.99 || item1.getDiscount() < 0.17){
+            if(item2.getDiscountQuantity() == 18){
+                System.out.println("Milk discount quantity equals 18");
+            }else{
+                System.out.println("Milk discount quantity low");
+            }
+        }else if(item3.getDiscount() < 0.01){
+            System.out.println("Water discount is less than 0.01. The discount is " + item3.getDiscount());
+        }else if(item3.getDiscount() < 0.05) {
+            System.out.println("Water discount is less than 0.05. The discount is " + item3.getDiscount());
+        }else if(item3.getDiscount() < 0.15) {
+            System.out.printf("Water discount is less than 0.015. The discount is %.3f", item3.getDiscount());
+        }//end of if statement
+
+    }//end of main
+
+
+
+    public static void yourInfoHeader(){
+        System.out.println("================================");
+        System.out.println("PROGRAMMER:  " + "Roberto Callejas");
+        System.out.println("PANTHER ID:  " + "6360177");
+        System.out.println();
+        System.out.println("CLASS \t\t COP2210 ");
+        System.out.println("SECTION: \t " + "U02C");
+        System.out.println("SEMESTER: \t " + "Summer 2023");
+        System.out.println("CLASS TIME: \t " + "10:00 AM - 12:50 PM");
+        System.out.println();
+        System.out.println("Assignment:  " + "Lab 6");
+        System.out.println();
+        System.out.println("CERTIFICATION: \nI understand FIU's academic policies and i certify");
+        System.out.println("that this work is my own and that none of it is the");
+        System.out.println("work of any other person");
+        System.out.println("================================");
+        System.out.println();
+
+    }//end of yourInfoHeader
+
+}//end of Controller
--- src/things/Items.java
+++ src/things/Items.java
@@ -1,2 +1,74 @@
-package things;public class Items {
-}
+package things;
+
+public class Items  {
+
+    private String name;
+    private double price;
+    private int discountQuantity;
+    private double discount;
+
+
+    public Items(String name, double price){
+        this.name = name;
+        this.price = price;
+        discountQuantity = 20;
+        discount = 0.2;
+
+    }//end of items constructor
+
+    public Items(String name, double price, int discountQuantity, double discount){
+        this.name = name;
+        this.price = price;
+        this.discountQuantity = discountQuantity;
+        this.discount = discount;
+
+    }//end of items constructor
+
+    public String getName(){
+        return name;
+    }//end of getName
+    public void setName(String name){
+        this.name = name;
+    }//end of setName
+    public double getPrice(){
+        return price;
+    }//end of getPrice
+    public void setPrice(double price){
+        this.price = price;
+    }//end of setPrice
+    public int getDiscountQuantity(){
+        return discountQuantity;
+    }//end of getDiscountQuantity
+    public void setDiscountQuantity(int discountQuantity){
+        this.discountQuantity = discountQuantity;
+    }//end of setDiscountQuantity
+    public double getDiscount(){
+        return discount;
+    }//end of getDiscount
+    public void setDiscountPercentage(double discount){
+        this.discount = discount;
+    }//end of setDiscountPercentage
+    public void info (boolean showHeader){
+
+        if(showHeader == true){
+            System.out.println("--------------------------------");
+            System.out.println("Info: \t\t\t\t" + name);
+            System.out.println("--------------------------------");
+            System.out.println("Name: \t\t\t\t" + name);
+            System.out.println("Price: \t\t\t\t" + price);
+            System.out.println("discountQuantity: \t" + discountQuantity);
+            System.out.printf("discount: \t\t\t%.3f\n", discount);
+
+        }else{
+            System.out.println("Name: \t\t\t\t" + name);
+            System.out.println("Price: \t\t\t\t" + price);
+            System.out.println("discountQuantity: \t" + discountQuantity);
+            System.out.printf("discount: \t\t\t%.3f\n", discount);
+        }
+
+
+    }//end of info
+
+
+
+}//end of items
